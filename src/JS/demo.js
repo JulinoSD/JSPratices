014898b219f7289f1b8cf5a22635c9fd4c165ec1
Julino Segunda Dídimo <ddimojulino@gmail.com>
2020-11-06 10:52:09 -0300

    Starting through by the end

---
+++ src/JS/demo.js
@@ -0,0 +1,18 @@
+console.log('Hello world')
+
+let h = 'Hello'
+let w = 'world suck'
+
+console.log('%s, %s', h, w)
+
+/*--------------------
+Reverse string
+--------------------*/
+let tri = 'Oi pai!'
+let inv = ""
+
+tri.split("").forEach((revi) => {
+    inv = revi + inv
+})
+
+console.log(inv)
